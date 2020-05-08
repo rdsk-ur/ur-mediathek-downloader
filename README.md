dc39f4ff3edc6b2bf343d85c707d6991d2bad34a
Lolopretjzyl <Lolopretjzyl@gmail.com>
2020-05-08 11:01:20 +0200

    add video downloader

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
 # Tools to download stuff from the UR mediathek
 
+## `download_video.py`
+
+Downloads a video file from the UR Medithek.
+
+### Installation
+
+1. Install the [selenium-wire](https://github.com/wkeeling/selenium-wire) package from pip.
+2. Install [youtube-dl](http://ytdl-org.github.io/youtube-dl/)
+3. You need to have a `credentials.json` file in the current working directory, which should look like this:
+
+    ``` json
+    {
+        "account": "vip12345",
+        "password": "correcthorsebatterystaple"
+    }
+    ```
+
+Well, replace the entries with your own login information.
+
+### Usage
+
+    python3 download_video.py https://mediathek2.uni-regensburg.de/playthis/<VIDEO_ID>
+
+The video file will be downloaded and saved in the current working directory.
+
 ## `download_audio.py`
 
 Downloads all audio files from a given channel and stores them in the current directory.
@@ -14,6 +39,3 @@ Install the following python packages:
 ### Usage
 
     python3 download_audio.py https://mediathek2.uni-regensburg.de/list/<CHANNEL_ID>
-
-(Replace `<CHANNEL_ID>` with your channel)
-
---
+++ download_video.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import json
+from argparse import ArgumentParser
+from seleniumwire import webdriver
+from selenium.common.exceptions import TimeoutException
+
+def main(video_url, timeout=20):
+    try:
+        with open("credentials.json") as credentials_file:
+            credentials = json.load(credentials_file)
+    except FileNotFoundError:
+        print("credentials.json is missing. Please read the readme for more details")
+        return
+    except json.decoder.JSONDecodeError:
+        print("credentials.json is no valid JSON file")
+        return
+
+    with webdriver.Firefox() as driver:
+        driver.get(sys.argv[1])
+
+        print("Enter credentials")
+        acount_name = credentials["account"]
+        password = credentials["password"]
+        driver.execute_script(f"document.querySelector('#frmLoginRzaccout').value='{acount_name}'")
+        driver.execute_script(f"document.querySelector('#frmLoginPassword').value='{password}'")
+        driver.execute_script('document.querySelector("form.form-signin button").click()')
+
+        print("Wait for manifest request")
+        try:
+            req = driver.wait_for_request("manifest.mpd", timeout)
+            manifest_url = req.path
+        except TimeoutException:
+            print("Did not receive the manifest after", timeout, "seconds")
+            print("Consider increasing the timeout by setting the --timeout option")
+            return
+
+        video_name = driver.find_element_by_css_selector("h5.card-title").text
+
+    if manifest_url is None:
+        print("Could not find a manifest")
+    else:
+        print("Video title:", video_name)
+        print("Manifest URL:", manifest_url)
+        print("Starting youtube-dl")
+        os.system(f"youtube-dl '{manifest_url}'")
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="""A tool to download videos from the UR Mediathek.
+    To use it, you must have a credentials.json file in the current directory which contains the keys 'account' and 'password'.
+    """)
+    parser.add_argument("url", help="The URL of the site that shows the video. Usually begins with https://mediathek2.uni-regensburg.de/playthis/")
+    parser.add_argument("--timeout", default=20, help="The number of seconds that will be waited until the manifest is requested.")
+
+    args = parser.parse_args()
+    main(args.url, args.timeout)
