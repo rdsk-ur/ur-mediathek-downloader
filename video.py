b68da6970b9a6a25bd76c281c1ccb7f3c1cd2c9d
trivicious <jonas.sueskind@gmail.com>
2020-10-26 18:18:47 +0100

    Adjust video.py
    Now use authenticated session instead of selenium webdriver. Also use subprocess.run instead of os.system.

--- video.py
+++ video.py
@@ -1,71 +1,38 @@
 #!/usr/bin/env python3
 
-import os
+import re
+import subprocess
 from argparse import ArgumentParser
 
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.firefox.options import Options
-from seleniumwire import webdriver
 
-from util import authenticate, get_credentials
-
-
-def download_video(video_url, driver, timeout=20, title=None, skip_auth=False):
-    if not skip_auth:
-        try:
-            credentials = get_credentials()
-        except:
-            return
-
-    del driver.requests
-    driver.get(video_url)
-
-    if not skip_auth:
-        authenticate(driver, credentials)
-
-    print("Wait for manifest request")
-    try:
-        req = driver.wait_for_request("manifest.mpd", timeout)
-        manifest_url = req.path
-    except TimeoutException:
-        print("Did not receive the manifest after", timeout, "seconds")
-        print("Are the provided credentials still valid?")
-        print("Alternatively, consider increasing the timeout by setting the --timeout option")
-        return
+def download_video(video_url, session, title=None):
+    res = session.get(video_url)
+    manifest_url = re.findall(r"src: '(.*manifest.*)'", res.text)[0]
 
     if title is None:
-        video_name = driver.find_element_by_css_selector("h5.card-title").text
+        onelined = re.sub(r"\s+", " ", res.text)
+        video_name = re.findall(r"<div class=\"card-header\">\s*<h3>(.*)</h3>", onelined)[0]
     else:
         video_name = title
 
-    if manifest_url is None:
-        print("Could not find a manifest")
-    else:
-        print("Video title:", video_name)
-        print("Manifest URL:", manifest_url)
-        print("Starting youtube-dl")
-        status = os.system(f"youtube-dl -o '{video_name}.mp4' '{manifest_url}'")
-        if status != 0:
-            raise ChildProcessError()
+    print("Video title:", video_name)
+    print("Manifest URL:", manifest_url)
+    print("Starting youtube-dl")
+    subprocess.run(["youtube-dl", "-o", f"{video_name}.mp4", manifest_url])
 
 
 if __name__ == "__main__":
+    from util import get_authenticated_session, get_credentials
+
     parser = ArgumentParser(description="""A tool to download videos from the UR Mediathek.
     To use it, you must have a credentials.json file in the current directory which contains the keys 'account' and 'password'.
     """)
     parser.add_argument("url",
                         help="The URL of the site that shows the video. Usually begins with https://mediathek2.uni-regensburg.de/playthis/")
-    parser.add_argument("--timeout", type=int, default=20,
-                        help="The number of seconds that will be waited until the manifest is requested.")
     parser.add_argument("--title", default=None, help="Overwrite the title derived from the mediathek.")
 
     args = parser.parse_args()
 
-    print("Launch headless firefox")
-    options = Options()
-    options.headless = True
-    with webdriver.Firefox(options=options) as driver:
-        try:
-            download_video(args.url, driver, args.timeout, args.title)
-        except ChildProcessError:
-            print("youtube-dl failed, please try again")
+    print("Starting session")
+    session = get_authenticated_session(get_credentials())
+    download_video(args.url, session, args.title)
